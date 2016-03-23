90aa94b1de967eb5eda0073c01515ff9c0dcc310
CapCarlos <afa60041@gmail.com>
2016-03-24 02:05:25 +0800

    20160324

--- WebApplication3/Models/Order.cs
+++ WebApplication3/Models/Order.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace WebApplication3.Models
 {
@@ -11,7 +14,8 @@ namespace WebApplication3.Models
         /// 訂單編號
         /// </summary>
 
-
+        [DisplayName("訂單編號")]
+        [Required()]
         public int OrderId { get; set; }
 
 
@@ -19,7 +23,7 @@ namespace WebApplication3.Models
         /// <summary>
         /// 客戶名稱
         /// </summary>
-
+        [DisplayName("客戶名稱")]
         public string CustName { get; set; }
 
 
@@ -28,7 +32,7 @@ namespace WebApplication3.Models
         /// 業務(員工姓名)
         /// </summary>
         /// 
-
+        [DisplayName("負責員工")]
         public string EmpName { get; set; }
 
 
@@ -37,7 +41,7 @@ namespace WebApplication3.Models
         /// 出貨公司名稱
         /// </summary>
         /// 
-
+        [DisplayName("出貨公司")]
         public string ShipperName { get; set; }
 
 
@@ -45,7 +49,7 @@ namespace WebApplication3.Models
         /// 訂單日期
         /// </summary>
         /// 
-
+        [DisplayName("訂購日期")]
         public DateTime? Orderdate { get; set; }
 
 
@@ -54,7 +58,7 @@ namespace WebApplication3.Models
         /// 出貨日期
         /// </summary>
         /// 
-
+        [DisplayName("出貨日期")]
         public DateTime? ShippedDate { get; set; }
 
 
@@ -62,7 +66,7 @@ namespace WebApplication3.Models
         /// 需要日期
         /// </summary>
         /// 
-
+        [DisplayName("需要日期")]
         public DateTime? RequireDdate { get; set; }
 
 
