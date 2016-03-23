63ed5dd2cf31bf3fbf515e7e77d531cd1728c19a
CapCarlos <afa60041@gmail.com>
2016-03-24 01:33:35 +0800

    test2

---
+++ WebApplication3/Models/Order.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Order
+    {
+        /// <summary>
+        /// 訂單編號
+        /// </summary>
+
+
+        public int OrderId { get; set; }
+
+
+
+        /// <summary>
+        /// 客戶名稱
+        /// </summary>
+
+        public string CustName { get; set; }
+
+
+
+        /// <summary>
+        /// 業務(員工姓名)
+        /// </summary>
+        /// 
+
+        public string EmpName { get; set; }
+
+
+
+        /// <summary>
+        /// 出貨公司名稱
+        /// </summary>
+        /// 
+
+        public string ShipperName { get; set; }
+
+
+        /// <summary>
+        /// 訂單日期
+        /// </summary>
+        /// 
+
+        public DateTime? Orderdate { get; set; }
+
+
+
+        /// <summary>
+        /// 出貨日期
+        /// </summary>
+        /// 
+
+        public DateTime? ShippedDate { get; set; }
+
+
+        /// <summary>
+        /// 需要日期
+        /// </summary>
+        /// 
+
+        public DateTime? RequireDdate { get; set; }
+
+
+    }
+}
---
+++ WebApplication3/Models/OrderService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class OrderService
+    {
+
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder()
+        {
+
+
+        }
+        /// <summary>
+		/// 查詢訂單(編號)
+		/// </summary>
+        public Models.Order GetOrderById(string id)
+        {
+
+            return new Models.Order();
+        }
+
+    }
+}
